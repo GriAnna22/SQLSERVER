2b5b728f4a259de74d68f95f8ba3aa0096e22c19
GriAnna22 <92019410+GriAnna22@users.noreply.github.com>
2022-05-30 00:59:30 +0300

    Add files via upload

---
+++ Models/Models/StatusAgreement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DB2
+{
+    public partial class StatusAgreement
+    {
+        public StatusAgreement()
+        {
+            Agreements = new HashSet<Agreement>();
+        }
+
+        public int Id { get; set; }
+        public string Status { get; set; }
+
+        public virtual ICollection<Agreement> Agreements { get; set; }
+    }
+}
